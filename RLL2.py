c213840b3e6145a6ec8c11e7ccedbd1a0ff7cdc3
prince jain <55597203+princ1211@users.noreply.github.com>
2022-01-11 06:54:35 +0000

    Problems Solved

---
+++ RLL2.py
@@ -0,0 +1,13 @@
+ def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        prev = None
+        current = head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        head = prev
+        return head
+        
+        
