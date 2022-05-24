b76dcbff09ae64befb994b112aec15b52692b632
Prince Jain <55597203+princ1211@users.noreply.github.com>
2022-05-24 18:54:01 +0000

    Problem Solved

---
+++ main.py
@@ -0,0 +1,5 @@
+a=[ "nmhed", "ouanm", "ytoxq", "vefsd", "chaka", "sygei", "xqdrk", "kmdpv", "pyodz", "kdogm", "mydfi", "qdqqu", "gmkfa", "xldyk", "dxceb", "jgdul", "jlvyq", "frxgw", "mnpvt", "myqlv" ]
+for i in range(len(a)):
+  a[0]+=a[i]
+b=set(a[0].lower())
+print(len(b))
